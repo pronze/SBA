2c00f43cac70e726dfe2f61ac70b90ef6efcdd0a
pronze <pranoysuresh123@gmail.com>
2020-07-10 15:45:33 +0400

    simplified Parties

--- src/main/java/org/pronze/hypixelify/listener/PartyListener.java
+++ src/main/java/org/pronze/hypixelify/listener/PartyListener.java
@@ -5,18 +5,14 @@ import com.alessiodp.parties.api.interfaces.PartiesAPI;
 import com.alessiodp.parties.api.interfaces.Party;
 import com.alessiodp.parties.api.interfaces.PartyPlayer;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.pronze.hypixelify.Hypixelify;
-import org.pronze.hypixelify.event.PartyLeaderGameJoinEvent;
-import org.pronze.hypixelify.event.PartyLeaderGameLeaveEvent;
 import org.screamingsandals.bedwars.Main;
 import org.screamingsandals.bedwars.api.BedwarsAPI;
 import org.screamingsandals.bedwars.api.events.BedwarsPlayerJoinEvent;
 import org.screamingsandals.bedwars.api.events.BedwarsPlayerLeaveEvent;
 import org.screamingsandals.bedwars.api.game.Game;
-import org.screamingsandals.bedwars.api.game.GameStatus;
 import static org.pronze.hypixelify.utils.PartyUtil.checkPartyLeader;
 
 public class PartyListener implements Listener {
@@ -29,9 +25,18 @@ public class PartyListener implements Listener {
     public void onBwPlayerJoin(BedwarsPlayerJoinEvent e){
         PartiesAPI api = Parties.getApi();
         PartyPlayer player = api.getPartyPlayer(e.getPlayer().getUniqueId());
+        Game game = e.getGame();
         if(checkPartyLeader(player)) {
-            PartyLeaderGameJoinEvent event = new PartyLeaderGameJoinEvent(player.getPlayerUUID(), e.getGame(), api.getParty(player.getPartyName()));
-            Bukkit.getPluginManager().callEvent(event);
+            Party party = api.getParty(player.getPartyName());
+            if(party == null) return;
+
+            for(PartyPlayer p : party.getOnlineMembers(true)){
+                if(!p.getPlayerUUID().equals(player.getPlayerUUID())){
+                    Main.getGame(game.getName()).joinToGame(Bukkit.getPlayer(p.getPlayerUUID()));
+                    if(BedwarsAPI.getInstance().isPlayerPlayingAnyGame(e.getPlayer()) && game.isPlayerInAnyTeam(e.getPlayer()) && e.getGame().getTeamOfPlayer(e.getPlayer()).getMaxPlayers() > e.getGame().getTeamOfPlayer(e.getPlayer()).countConnectedPlayers())
+                        e.getGame().selectPlayerTeam(Bukkit.getPlayer(p.getPlayerUUID()), game.getTeamOfPlayer(e.getPlayer()));
+                }
+            }
         }
     }
 
@@ -44,46 +49,14 @@ public class PartyListener implements Listener {
         if(!e.getPlayer().isOnline())
             return;
         if(checkPartyLeader(player)){
-            PartyLeaderGameLeaveEvent event = new PartyLeaderGameLeaveEvent(player.getPlayerUUID(), api.getParty(player.getPartyName()));
-            Bukkit.getPluginManager().callEvent(event);
-        }
-    }
+            Party party = api.getParty(player.getPartyName());
+            if(party != null  && party.getMembers().size() <= 1) return;
 
-    @EventHandler
-    public void PartyLeaderLeave(PartyLeaderGameLeaveEvent e){
-        Player partyleader = e.getPartyLeader();
-        Party party = e.getParty();
-
-        if(party == null) return;
-        if(e.isCancelled()) return;
-
-        if(party.getMembers().size() <= 1) return;
-
-        for (PartyPlayer p : party.getOnlineMembers(true)){
-            if(!p.getPlayerUUID().equals(partyleader.getUniqueId()) && Bukkit.getPlayer(p.getPlayerUUID()).isOnline() && BedwarsAPI.getInstance().isPlayerPlayingAnyGame(Bukkit.getPlayer(p.getPlayerUUID()))){
-                Main.getGame(BedwarsAPI.getInstance().getGameOfPlayer(Bukkit.getPlayer(p.getPlayerUUID())).getName()).leaveFromGame(Bukkit.getPlayer(p.getPlayerUUID()));
-            }
-        }
-
-    }
-
-    @EventHandler
-    public void PartyLeaderJoin(PartyLeaderGameJoinEvent e){
-        Player partyleader = e.getPartyLeader();
-        Game game = e.getGame();
-        Party party = e.getParty();
-
-        if(e.isCancelled())
-            return;
-
-        if(game.getStatus() != GameStatus.RUNNING || !partyleader.isOnline())
-            return;
-
-        for(PartyPlayer p : party.getOnlineMembers(true)){
-            if(!p.getPlayerUUID().equals(partyleader.getUniqueId())){
-                Main.getGame(game.getName()).joinToGame(Bukkit.getPlayer(p.getPlayerUUID()));
-                if(BedwarsAPI.getInstance().isPlayerPlayingAnyGame(partyleader) && game.isPlayerInAnyTeam(partyleader) && e.getGame().getTeamOfPlayer(partyleader).getMaxPlayers() > e.getGame().getTeamOfPlayer(partyleader).countConnectedPlayers())
-                    e.getGame().selectPlayerTeam(Bukkit.getPlayer(p.getPlayerUUID()), game.getTeamOfPlayer(partyleader));
+            assert party != null;
+            for (PartyPlayer p : party.getOnlineMembers(true)){
+                if(!p.getPlayerUUID().equals(player.getPlayerUUID()) && Bukkit.getPlayer(p.getPlayerUUID()).isOnline() && BedwarsAPI.getInstance().isPlayerPlayingAnyGame(Bukkit.getPlayer(p.getPlayerUUID()))){
+                    Main.getGame(BedwarsAPI.getInstance().getGameOfPlayer(Bukkit.getPlayer(p.getPlayerUUID())).getName()).leaveFromGame(Bukkit.getPlayer(p.getPlayerUUID()));
+                }
             }
         }
     }
--- src/main/java/org/pronze/hypixelify/utils/PartyUtil.java
+++ src/main/java/org/pronze/hypixelify/utils/PartyUtil.java
@@ -16,8 +16,6 @@ public class PartyUtil {
                     return true;
             }
         }
-
-
         return false;
     }
 
