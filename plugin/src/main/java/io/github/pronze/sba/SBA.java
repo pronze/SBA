dab73cbb7992da81c402a41a705fc2712d634626
pronze <pronzemc@gmail.com>
2021-08-09 19:16:14 +0400

    Fix for build, possible fix for generator splitting

--- plugin/src/main/java/io/github/pronze/sba/SBA.java
+++ plugin/src/main/java/io/github/pronze/sba/SBA.java
@@ -1,46 +1,46 @@
 package io.github.pronze.sba;
 
 import io.github.pronze.sba.commands.CommandManager;
-import io.github.pronze.sba.game.ArenaManager;
-import io.github.pronze.sba.game.tasks.GameTaskManager;
-import io.github.pronze.sba.manager.*;
 import io.github.pronze.sba.config.IConfigurator;
+import io.github.pronze.sba.config.SBAConfig;
+import io.github.pronze.sba.game.ArenaManager;
 import io.github.pronze.sba.game.IGameStorage;
+import io.github.pronze.sba.game.tasks.GameTaskManager;
 import io.github.pronze.sba.inventories.GamesInventory;
 import io.github.pronze.sba.inventories.SBAStoreInventory;
 import io.github.pronze.sba.inventories.SBAUpgradeStoreInventory;
 import io.github.pronze.sba.lang.ILanguageService;
 import io.github.pronze.sba.lib.lang.LanguageService;
 import io.github.pronze.sba.listener.*;
+import io.github.pronze.sba.manager.IArenaManager;
+import io.github.pronze.sba.manager.IPartyManager;
 import io.github.pronze.sba.party.PartyManager;
 import io.github.pronze.sba.placeholderapi.SBAExpansion;
 import io.github.pronze.sba.service.*;
 import io.github.pronze.sba.specials.listener.BridgeEggListener;
 import io.github.pronze.sba.specials.listener.PopupTowerListener;
-import io.github.pronze.sba.utils.FirstStartConfigReplacer;
 import io.github.pronze.sba.utils.DateUtils;
+import io.github.pronze.sba.utils.FirstStartConfigReplacer;
 import io.github.pronze.sba.utils.Logger;
 import io.github.pronze.sba.visuals.LobbyScoreboardManager;
 import io.github.pronze.sba.visuals.MainLobbyVisualsManager;
 import io.github.pronze.sba.wrapper.PlayerWrapper;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.screamingsandals.bedwars.Main;
+import org.screamingsandals.bedwars.api.BedwarsAPI;
 import org.screamingsandals.bedwars.api.game.Game;
-import org.screamingsandals.lib.Core;
+import org.screamingsandals.bedwars.lib.sgui.listeners.InventoryListener;
 import org.screamingsandals.lib.event.EventManager;
 import org.screamingsandals.lib.healthindicator.HealthIndicatorManager;
 import org.screamingsandals.lib.hologram.HologramManager;
 import org.screamingsandals.lib.npc.NPCManager;
 import org.screamingsandals.lib.packet.PacketMapper;
-import io.github.pronze.sba.config.SBAConfig;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.ServicePriority;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.screamingsandals.bedwars.Main;
-import org.screamingsandals.bedwars.api.BedwarsAPI;
-import org.screamingsandals.bedwars.lib.sgui.listeners.InventoryListener;
 import org.screamingsandals.lib.plugin.PluginContainer;
 import org.screamingsandals.lib.tasker.Tasker;
 import org.screamingsandals.lib.utils.PlatformType;
@@ -50,13 +50,16 @@ import org.screamingsandals.lib.utils.annotations.PluginDependencies;
 import org.screamingsandals.lib.utils.annotations.methods.OnPostEnable;
 import org.screamingsandals.simpleinventories.SimpleInventoriesCore;
 import pronze.lib.scoreboards.ScoreboardManager;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static io.github.pronze.sba.utils.MessageUtils.showErrorMessage;
 
 @Plugin(
         id = "SBA",
-        authors = { "pronze" },
+        authors = {"pronze"},
         loadTime = Plugin.LoadTime.POSTWORLD,
         version = "1.5.6-SNAPSHOT"
 )
@@ -66,14 +69,13 @@ import static io.github.pronze.sba.utils.MessageUtils.showErrorMessage;
         "PlaceholderAPI"
 )
 @Init(services = {
-        Core.class,
         Tasker.class,
         PacketMapper.class,
         HologramManager.class,
         HealthIndicatorManager.class,
         SimpleInventoriesCore.class,
         NPCManager.class,
-        EventManager.class,
+        //EventManager.class,
         UpdateChecker.class,
         SBAConfig.class,
         Logger.class,
@@ -108,6 +110,7 @@ import static io.github.pronze.sba.utils.MessageUtils.showErrorMessage;
 
 public class SBA extends PluginContainer implements AddonAPI {
     private static SBA instance;
+    private final List<Listener> registeredListeners = new ArrayList<>();
 
     public static SBA getInstance() {
         return instance;
@@ -116,8 +119,6 @@ public class SBA extends PluginContainer implements AddonAPI {
     public static JavaPlugin getPluginInstance() {
         return instance.getPluginDescription().as(JavaPlugin.class);
     }
-
-    private final List<Listener> registeredListeners = new ArrayList<>();
 
     @Override
     public void load() {
--- plugin/src/main/java/io/github/pronze/sba/listener/GeneratorSplitterListener.java
+++ plugin/src/main/java/io/github/pronze/sba/listener/GeneratorSplitterListener.java
@@ -1,12 +1,19 @@
 package io.github.pronze.sba.listener;
 
+import io.github.pronze.sba.SBA;
 import io.github.pronze.sba.config.SBAConfig;
 import lombok.SneakyThrows;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.screamingsandals.bedwars.Main;
 import org.screamingsandals.bedwars.utils.Sounds;
+import org.screamingsandals.lib.bukkit.utils.nms.Version;
 import org.screamingsandals.lib.event.OnEvent;
 import org.screamingsandals.lib.event.player.SPlayerPickupItemEvent;
 import org.screamingsandals.lib.utils.annotations.Service;
@@ -24,52 +31,72 @@ public class GeneratorSplitterListener {
     @SneakyThrows
     @OnPostEnable
     public void onPostEnable() {
+        if (Version.isVersion(1, 12)) {
+            SBA.getInstance().registerListener(new GeneratorSplitterListener112());
+        } else {
+            SBA.getInstance().registerListener(new GeneratorSplitterListenerBefore112());
+        }
         allowedMaterials = Objects.requireNonNull(SBAConfig.getInstance().node("generator-splitter", "allowed-materials").
-                getList(String.class))
+                        getList(String.class))
                 .stream()
                 .map(matName -> Material.valueOf(matName.toUpperCase().trim()))
                 .collect(Collectors.toList());
     }
 
     @SuppressWarnings("unchecked")
-    @OnEvent
-    public void onItemPickup(SPlayerPickupItemEvent event) {
-        final var bukkitPlayer = event.getPlayer().as(Player.class);
-        if (!Main.isPlayerInGame(bukkitPlayer)) {
-            return;
+    @SneakyThrows
+    public static void onPickup(Player player, Item item) {
+        if (Main.isPlayerInGame(player)) {
+            final var game = Main.getInstance().getGameOfPlayer(player);
+            final var playerTeam = game.getTeamOfPlayer(player);
+
+            final var isSpawnedItem = game.getItemSpawners()
+                    .stream()
+                    .map(itemSpawner -> (List<Item>) Reflect.getField(itemSpawner, "spawnedItems"))
+                    .anyMatch(items -> items.contains(item));
+
+            if (!isSpawnedItem) {
+                return;
+            }
+
+            if (!allowedMaterials.contains(item.getItemStack().getType())) {
+                return;
+            }
+
+            player.getWorld().getNearbyEntities(player.getLocation(), 3, 3, 3)
+                    .stream()
+                    .filter(entity -> !entity.equals(player))
+                    .filter(entity -> entity instanceof Player)
+                    .map(entity -> (Player) entity)
+                    .filter(Main::isPlayerInGame)
+                    .forEach(nearbyPlayer -> {
+                        final var nearbyPlayerTeam = game.getTeamOfPlayer(nearbyPlayer);
+                        if (nearbyPlayerTeam == playerTeam) {
+                            nearbyPlayer.getInventory().addItem(item.getItemStack().clone());
+                            Sounds.playSound(nearbyPlayer, nearbyPlayer.getLocation(),
+                                    "ENTITY_ITEM_PICKUP",
+                                    Sounds.ENTITY_ITEM_PICKUP, 1, 1);
+                        }
+                    });
         }
+    }
 
-        final var game = Main.getInstance().getGameOfPlayer(bukkitPlayer);
-        final var playerTeam = game.getTeamOfPlayer(bukkitPlayer);
-        final var bukkitItem = event.getItem().as(Item.class);
+    private static class GeneratorSplitterListener112 implements Listener {
 
-        final var isSpawnedItem = game.getItemSpawners()
-                .stream()
-                .map(itemSpawner -> (List<Item>) Reflect.getField(itemSpawner, "spawnedItems"))
-                .anyMatch(items -> items.contains(bukkitItem));
-
-        if (!isSpawnedItem) {
-            return;
+        @SneakyThrows
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onPickup(EntityPickupItemEvent event) {
+            if (!((event.getEntity()) instanceof Player)) {
+                return;
+            }
+            GeneratorSplitterListener.onPickup((Player) event.getEntity(), event.getItem());
         }
+    }
 
-        if (!allowedMaterials.contains(bukkitItem.getItemStack().getType())) {
-            return;
+    private static class GeneratorSplitterListenerBefore112 implements Listener {
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onPickup(PlayerPickupItemEvent event) {
+            GeneratorSplitterListener.onPickup(event.getPlayer(), event.getItem());
         }
-
-        bukkitPlayer.getWorld().getNearbyEntities(bukkitPlayer.getLocation(), 3, 3, 3)
-                .stream()
-                .filter(entity -> !entity.equals(bukkitPlayer))
-                .filter(entity -> entity instanceof Player)
-                .map(entity -> (Player) entity)
-                .filter(Main::isPlayerInGame)
-                .forEach(nearbyPlayer -> {
-                    final var nearbyPlayerTeam = game.getTeamOfPlayer(nearbyPlayer);
-                    if (nearbyPlayerTeam == playerTeam) {
-                        nearbyPlayer.getInventory().addItem(bukkitItem.getItemStack().clone());
-                        Sounds.playSound(nearbyPlayer, nearbyPlayer.getLocation(),
-                                "ENTITY_ITEM_PICKUP",
-                                Sounds.ENTITY_ITEM_PICKUP, 1, 1);
-                    }
-                });
     }
 }
