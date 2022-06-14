473cda6fadb4c0f91075dec2a91a18bdd122cc9e
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2022-06-14 15:57:24 -0400

    AntiCheat support

--- plugin/src/main/java/io/github/pronze/sba/listener/ExplosionVelocityControlListener.java
+++ plugin/src/main/java/io/github/pronze/sba/listener/ExplosionVelocityControlListener.java
@@ -1,4 +1,5 @@
 package io.github.pronze.sba.listener;
+
 import io.github.pronze.sba.SBA;
 import io.github.pronze.sba.config.SBAConfig;
 import io.github.pronze.sba.service.AntiCheatIntegration;
@@ -13,16 +14,22 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 import org.screamingsandals.bedwars.Main;
 import org.screamingsandals.lib.utils.annotations.Service;
 import org.screamingsandals.lib.utils.annotations.methods.OnPostEnable;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @Service
 public class ExplosionVelocityControlListener implements Listener {
-    private final Set<Player> explosionAffectedPlayers = new HashSet<>();
+    private final Map<Player, BukkitTask> explosionAffectedPlayers = new HashMap<>();
 
     @OnPostEnable
     public void postEnable() {
@@ -35,13 +42,22 @@ public class ExplosionVelocityControlListener implements Listener {
             final var entity = event.getEntity();
             if (entity instanceof Player) {
                 final var player = (Player) entity;
-                if (explosionAffectedPlayers.contains(player)) {
-                    event.setDamage(SBAConfig.getInstance().node("tnt-fireball-jumping", "fall-damage").getDouble(3.0D));
-                    explosionAffectedPlayers.remove(player);
-                    AntiCheatIntegration.getInstance().endTntJump(player);
+                if (explosionAffectedPlayers.containsKey(player)) {
+                    event.setDamage(
+                            SBAConfig.getInstance().node("tnt-fireball-jumping", "fall-damage").getDouble(3.0D));
+                    Logger.trace("Landing tnt jump from fall damage", player);
+                    endTntJump(player);
                 }
             }
         }
+    }
+
+    private void endTntJump(Player player) {
+        BukkitTask potentialTask = explosionAffectedPlayers.get(player);
+        if (potentialTask != null)
+            potentialTask.cancel();
+        explosionAffectedPlayers.remove(player);
+        AntiCheatIntegration.getInstance().tntJumpLanding(player);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -49,30 +65,37 @@ public class ExplosionVelocityControlListener implements Listener {
         final var explodedEntity = event.getDamager();
 
         if (explodedEntity instanceof Explosive) {
-            final var detectionDistance = SBAConfig.getInstance().node("tnt-fireball-jumping", "detection-distance").getDouble(5.0D);
+            final var detectionDistance = SBAConfig.getInstance().node("tnt-fireball-jumping", "detection-distance")
+                    .getDouble(5.0D);
 
-            explodedEntity.getWorld().getNearbyEntities(explodedEntity.getLocation(), detectionDistance, detectionDistance, detectionDistance)
+            explodedEntity.getWorld()
+                    .getNearbyEntities(explodedEntity.getLocation(), detectionDistance, detectionDistance,
+                            detectionDistance)
                     .stream()
-                    .filter(entity ->  !entity.equals(explodedEntity))
+                    .filter(entity -> !entity.equals(explodedEntity))
                     .forEach(entity -> {
                         Vector vector = explodedEntity
                                 .getLocation()
                                 .clone()
-                                .add(0, SBAConfig.getInstance().node("tnt-fireball-jumping", "acceleration-y").getDouble(1.0) ,0)
+                                .add(0, SBAConfig.getInstance().node("tnt-fireball-jumping", "acceleration-y")
+                                        .getDouble(1.0), 0)
                                 .toVector()
                                 .subtract(explodedEntity.getLocation().toVector()).normalize();
-                        vector.setY(vector.getY() /  SBAConfig.getInstance().node("tnt-fireball-jumping", "reduce-y").getDouble(2.0));
-                        vector.multiply(SBAConfig.getInstance().node("tnt-fireball-jumping", "launch-multiplier").getDouble(4.0));
+                        vector.setY(vector.getY()
+                                / SBAConfig.getInstance().node("tnt-fireball-jumping", "reduce-y").getDouble(2.0));
+                        vector.multiply(SBAConfig.getInstance().node("tnt-fireball-jumping", "launch-multiplier")
+                                .getDouble(4.0));
 
                         if (entity instanceof Player) {
                             final var player = (Player) entity;
                             if (player.getGameMode() == GameMode.SPECTATOR || !Main.isPlayerInGame(player)) {
                                 return;
                             }
-                            vector.add(new Vector(player.getEyeLocation().getDirection().getX(), 0, player.getEyeLocation().getDirection().getZ()));
+                            vector.add(new Vector(player.getEyeLocation().getDirection().getX(), 0,
+                                    player.getEyeLocation().getDirection().getZ()));
                             AntiCheatIntegration.getInstance().beginTntJump(player);
                             player.setVelocity(vector);
-                            explosionAffectedPlayers.add(player);
+                            explosionAffectedPlayers.put(player, startTask(player));
                             return;
                         }
 
@@ -81,5 +104,31 @@ public class ExplosionVelocityControlListener implements Listener {
                         }
                     });
         }
+    }
+
+    public BukkitTask startTask(Player player) {
+        BukkitTask previousTask = explosionAffectedPlayers.get(player);
+        if (previousTask != null)
+            previousTask.cancel();
+        return new BukkitRunnable() {
+            boolean onGround = false;
+            int count = 0;
+
+            @Override
+            public void run() {
+                if (player.isOnGround()) {
+                    onGround = true;
+                }
+                if (onGround) {
+                    count++;
+                }
+                if (count > 3) {
+                    this.cancel();
+                    Logger.trace("Landing tnt jump from being on the ground for 1.5sec", player);
+
+                    endTntJump(player);
+                }
+            }
+        }.runTaskTimer(SBA.getPluginInstance(), 20, 10);
     }
 }
--- plugin/src/main/java/io/github/pronze/sba/service/AntiCheatIntegration.java
+++ plugin/src/main/java/io/github/pronze/sba/service/AntiCheatIntegration.java
@@ -1,7 +1,9 @@
 package io.github.pronze.sba.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -9,14 +11,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.screamingsandals.lib.plugin.ServiceManager;
 import org.screamingsandals.lib.utils.annotations.Service;
 import org.screamingsandals.lib.utils.annotations.methods.OnPostEnable;
 
 import io.github.pronze.sba.SBA;
+import io.github.pronze.sba.utils.Logger;
+import lombok.NoArgsConstructor;
 import me.frep.vulcan.api.event.VulcanFlagEvent;
+import me.frep.vulcan.api.event.VulcanGhostBlockEvent;
 
 @Service
+@NoArgsConstructor
 public class AntiCheatIntegration implements Listener {
     ArrayList<Listener> integrations = new ArrayList<>();
 
@@ -25,38 +33,90 @@ public class AntiCheatIntegration implements Listener {
     }
 
     Set<UUID> currentlyJumping = new HashSet<>();
+    Map<UUID, BukkitTask> jumpingCooldown = new HashMap<>();
 
     public void beginTntJump(Player p) {
-        //if (!currentlyJumping.contains(p.getUniqueId()))
-        //    currentlyJumping.add(p.getUniqueId());
-    }
-    public void endTntJump(Player p)
-    {
-        //currentlyJumping.remove(p.getUniqueId());
+        if (!currentlyJumping.contains(p.getUniqueId())) {
+            if (jumpingCooldown.containsKey(p.getUniqueId())) {
+                try {
+                    jumpingCooldown.get(p.getUniqueId()).cancel();
+                } catch (IllegalStateException ex) {
+
+                }
+                jumpingCooldown.remove(p.getUniqueId());
+            }
+            currentlyJumping.add(p.getUniqueId());
+            Logger.trace("Starting tnt jump for {}", p);
+        }
     }
 
-    public AntiCheatIntegration() {
+    public void tntJumpLanding(Player p) {
+        var id = p.getUniqueId();
+        currentlyJumping.remove(id);
+        Logger.trace("Landed tnt jump for {}", p);
+        jumpingCooldown.put(id,
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        jumpingCooldown.remove(id);
+                        Logger.trace("Ending tnt jump for {}", p);
+                    }
 
+                    @Override
+                    public void cancel() {
+                        super.cancel();
+                        Logger.trace("Cancelling delay tnt jump for {}", p);
+                    }
+                }.runTaskLater(SBA.getPluginInstance(), 120));
     }
 
     @OnPostEnable
     public void onPostEnabled() {
-        //if (Bukkit.getPluginManager().isPluginEnabled("Vulcan")) {
-        //    integrations.add(new VulcanIntegration());
-        //}
+        if (Bukkit.getPluginManager().isPluginEnabled("Vulcan")) {
+            integrations.add(new VulcanIntegration());
+        }
+        if (Bukkit.getPluginManager().isPluginEnabled("Spartan")) {
+            integrations.add(new SpartanIntegration());
+        }
 
-        //integrations.forEach(SBA.getInstance()::registerListener);
+        integrations.forEach(SBA.getInstance()::registerListener);
     }
 
-    private class VulcanIntegration implements Listener {
-        public VulcanIntegration() {
+    @NoArgsConstructor
+    private class SpartanIntegration implements Listener {
+        @EventHandler
+        public void onDetection(me.vagdedes.spartan.api.PlayerViolationEvent event) {
+            Logger.trace("Spartan detection, [{}]", event.getHackType());
+            var id = event.getPlayer().getUniqueId();
+            if (currentlyJumping.contains(id) || jumpingCooldown.containsKey(id)) {
+                Logger.trace("Spartan detection cancelled, [{}]", event.getHackType());
+                event.setCancelled(true);
+            }
+        }
 
+        
+    }
+
+    @NoArgsConstructor
+    private class VulcanIntegration implements Listener {
+        @EventHandler
+        public void onGhostBlock(VulcanGhostBlockEvent event) {
+            var id = event.getPlayer().getUniqueId();
+            if (currentlyJumping.contains(id) || jumpingCooldown.containsKey(id)) {
+                event.setCancelled(true);
+            }
         }
 
         @EventHandler
         public void onVulcanDetection(VulcanFlagEvent event) {
-            //if (currentlyJumping.contains(event.getPlayer().getUniqueId()))
+            Logger.trace("Vulcan detection, [{},{}]", event.getCheck().getDisplayName(),
+                    event.getCheck().getDisplayType());
+            var id = event.getPlayer().getUniqueId();
+            if (currentlyJumping.contains(id) || jumpingCooldown.containsKey(id)) {
+                Logger.trace("Vulcan detection cancelled, {},{},{}", event.getInfo(), event.getCheck().getComplexType(),
+                        event.getCheck().getName());
                 event.setCancelled(true);
+            }
         }
     }
 }
