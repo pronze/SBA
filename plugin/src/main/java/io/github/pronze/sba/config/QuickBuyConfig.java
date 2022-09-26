2778ac8a11a5d46d695a62a654a676b1eb8a2879
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2022-09-26 07:32:12 -0400

    Quick buy

--- plugin/src/main/java/io/github/pronze/sba/config/QuickBuyConfig.java
+++ plugin/src/main/java/io/github/pronze/sba/config/QuickBuyConfig.java
@@ -1,12 +1,16 @@
 package io.github.pronze.sba.config;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.screamingsandals.lib.plugin.ServiceManager;
@@ -17,6 +21,8 @@ import io.github.pronze.sba.utils.Logger;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Service
@@ -37,11 +43,11 @@ public class QuickBuyConfig {
 
     @Data
     @Accessors(fluent = true)
-    @NoArgsConstructor
-    @AllArgsConstructor
+    @RequiredArgsConstructor
     public class UserQuickBuyConfig {
         private Map<String, QuickBuyItem> items = new HashMap<>();
-
+        @NonNull
+        private UUID userId;
         @Data
         @Accessors(fluent = true)
         @NoArgsConstructor
@@ -61,18 +67,19 @@ public class QuickBuyConfig {
             QuickBuyConfig.this.save(this);
             return this;
         }
-        public QuickBuyItem of(String id)
-        {
+
+        public QuickBuyItem of(String id) {
             return items.get(id);
         }
-        public UserQuickBuyConfig set(String id,QuickBuyItem val)
-        {
-            items.put(id,val);
+
+        public UserQuickBuyConfig set(String id, QuickBuyItem val) {
+            items.put(id, val);
             return this;
         }
+
         public UserQuickBuyConfig set(String quickBuyId, @NotNull Material type, Price price) {
-            QuickBuyItem item = new QuickBuyItem(type, price.getAmount(),price.getCurrency());
-            return set(quickBuyId,item);
+            QuickBuyItem item = new QuickBuyItem(type, price.getAmount(), price.getCurrency());
+            return set(quickBuyId, item);
         }
     }
 
@@ -80,7 +87,7 @@ public class QuickBuyConfig {
 
     public UserQuickBuyConfig of(OfflinePlayer op) {
         if (!config.containsKey(op.getUniqueId()))
-            config.put(op.getUniqueId(), new UserQuickBuyConfig());
+            config.put(op.getUniqueId(), new UserQuickBuyConfig(op.getUniqueId()));
         return config.get(op.getUniqueId());
     }
 
@@ -90,6 +97,23 @@ public class QuickBuyConfig {
         } catch (Throwable t) {
             Logger.error("Could not create quickbuy directory due to {}", t);
         }
+
+        Arrays.stream(directory.listFiles()).forEach(file->{
+            FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+            var fileName = file.getName();
+            String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
+            UserQuickBuyConfig userConfig = new UserQuickBuyConfig(UUID.fromString(fileNameWithoutExtension));
+
+            config.getKeys(false).forEach(key->{
+                var section = config.getConfigurationSection(key);
+                int amount = section.getInt("amount");
+                String resource = section.getString("resource");
+                String material = section.getString("material");
+                Material mat = Material.valueOf(material);
+                userConfig.set(key, mat, Price.of(amount,resource));
+            });
+            this.config.put(userConfig.userId(), userConfig);
+        });
     }
 
     public void save() {
@@ -102,7 +126,20 @@ public class QuickBuyConfig {
     }
 
     public void save(UserQuickBuyConfig userConfig) {
-
+        File file = new File(directory + File.separator + userConfig.userId()+".yml");
+        FileConfiguration config = new YamlConfiguration();
+        userConfig.items().forEach((section,content)->{
+            var sectionYaml = config.createSection(section);
+            sectionYaml.set("material", content.material.toString());
+            sectionYaml.set("amount", content.amount());
+            sectionYaml.set("resource", content.resource());
+        });
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
